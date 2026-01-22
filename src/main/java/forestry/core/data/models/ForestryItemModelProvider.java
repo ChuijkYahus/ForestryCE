5aaa52dce9be9850604290e30b460f022d52039d
Spearkiller <bradley56789@gmail.com>
2026-01-22 14:13:56 +1300

    fixed bog earth and humus crates not rendering properly

--- src/main/java/forestry/core/data/models/ForestryItemModelProvider.java
+++ src/main/java/forestry/core/data/models/ForestryItemModelProvider.java
@@ -82,6 +82,8 @@ public class ForestryItemModelProvider extends ItemModelProvider {
 		filledCrateModel(CrateItems.CRATED_MYCELIUM.getName(), mcLoc("block/mycelium_side"));
 		filledCrateModel(CrateItems.CRATED_GRASS_BLOCK.getName(), mcLoc("block/grass_block_top"));
 		filledCrateModel(CrateItems.CRATED_PROPOLIS.getName(), modLoc("item/propolis.0"));
+		filledCrateModel(CrateItems.CRATED_BOG_EARTH.getName(), modLoc("block/bog_earth.0"));
+		filledCrateModel(CrateItems.CRATED_HUMUS.getName(), modLoc("block/humus.0"));
 
 		for (Map.Entry<BlockTypePlanter, FeatureBlock<BlockPlanter, BlockItem>> cell : Iterables.concat(CultivationBlocks.MANAGED_PLANTER.getFeatureByType().entrySet(), CultivationBlocks.MANUAL_PLANTER.getFeatureByType().entrySet())) {
 			Block block = cell.getValue().block();
