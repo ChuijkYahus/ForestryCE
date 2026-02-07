e4205f197e472f72d6855381c00ab6e0ad89b6f6
Spearkiller <bradley56789@gmail.com>
2026-02-08 02:34:59 +1300

    Bog Earth can now be moistened by waterlogged blocks

--- src/main/java/forestry/core/blocks/BlockBogEarth.java
+++ src/main/java/forestry/core/blocks/BlockBogEarth.java
@@ -12,6 +12,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraftforge.common.IPlantable;
 
@@ -58,7 +59,7 @@ public class BlockBogEarth extends Block {
 		for (BlockPos waterPos : BlockPos.betweenClosed(pos.offset(-2, -2, -2), pos.offset(2, 2, 2))) {
 			BlockState blockState = world.getBlockState(waterPos);
 			Block block = blockState.getBlock();
-			if (block == Blocks.WATER) {
+			if (block == Blocks.WATER || blockState.hasProperty(BlockStateProperties.WATERLOGGED)) {
 				return true;
 			}
 		}
