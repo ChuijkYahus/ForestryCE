88d8c9307a5a94d1004f3c67c37bc4091fdf5527
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2026-08-11 18:29:41 -0700

    Removed unused imports from BlockWaterloggableTorch

--- src/main/java/forestry/core/blocks/BlockWaterloggableTorch.java
+++ src/main/java/forestry/core/blocks/BlockWaterloggableTorch.java
@@ -1,11 +1,8 @@
 package forestry.core.blocks;
 
-import net.minecraft.client.particle.DustParticle;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.DustParticleOptions;
-import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
@@ -16,7 +13,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import org.joml.Vector3f;
